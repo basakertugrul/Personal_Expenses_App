72edf26d129350a12f5b017e7bf84980cd49917e
basakertugrul <61563492+basakertugrul@users.noreply.github.com>
2020-08-26 18:12:52 +0300

    aciklama

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import './widgets/transaction_list.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -13,14 +14,52 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Center(
-        child: Text('Widget Playground!'),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text("CHART!"),
+              elevation: 5,
+              color: Colors.blue,
+            ),
+          ),
+          Card(
+            elevation: 5,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(labelText: "Title"),
+                      controller: titleController,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: "Amount"),
+                      controller: amountController,
+                    ),
+                    FlatButton(
+                      child: Text("Add Transactions"),
+                      textColor: Colors.purple,
+                      onPressed: () {},
+                    )
+                  ]),
+            ),
+          ),
+          TransactionList()
+        ],
       ),
     );
   }
